020283b899a38476be9d389d484ddf49d5ce9053
hanskoder963 <programful963@gmail.com>
2024-11-28 13:56:46 +0100

    gardin fulfort, logo+tekst, knap utsende

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello!');
+const menuButton =  document.querySelector('.menu-button');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+menuButton.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('active');
+});
